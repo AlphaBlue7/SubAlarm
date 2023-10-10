e9e4f0c46bb92e52bd61d53f4de50db7a2fa6f00
AlphaBlue7 <62275748+AlphaBlue7@users.noreply.github.com>
2023-10-10 22:40:22 +0530

    Update alert_sub.sh
    anew support removed.
    alternative difference identification methodology involves use of diff and sort
    sorting implement
    alternative difference identification methodology implemented

--- alert_sub.sh
+++ alert_sub.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-while true;
-do subfinder -silent -d targetdomain.com -all -o subop1.txt | anew subop.txt > newsubdomains.txt;
-subcount=$(cat newsubdomains.txt | wc -l)
 iterationcount=$(cat runcount.txt)
+cat subop.txt | sort > subop.txt
 
- if [ "$subcount" -ne 0 ]
-  then
-   echo "    telegram_format: 'You have $subcount new subdomains for tidal.com. The new subdomains are: {{data}}'" >> alertconfig.yaml;
-   notify -pc alertconfig.yaml -silent -data newsubdomains.txt  -provider telegram -bulk
+while true;
+do
+ subfinder -silent -d TARGETWEBSITE.com -all | sort > subop1.txt
+ diff subop.txt subop1.txt | grep '^<' | sed 's/^< //' > newsubdomains.txt
+ subcount=$(cat newsubdomains.txt | wc -l)
+
+ if [ "$subcount" -ne 0 ]; then
+  echo "    telegram_format: 'You have $subcount new subdomains for tidal.com. The new subdomains are: {{data}}'" >> alertconfig.yaml;
+  notify -pc alertconfig.yaml -silent -data newsubdomains.txt  -provider telegram -bulk
  fi
+ ((iterationcount++))
+ echo "$iterationcount" > runcount.txt
+ sleep 3600
 
-iterationcount=$((iterationcount + 1))
-echo "$iterationcount" > runcount.txt
-
-sleep 60m
 done

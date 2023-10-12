082554aca123af52f8cab72d8dcf4029d0a14d0f
AlphaBlue7 <62275748+AlphaBlue7@users.noreply.github.com>
2023-10-13 00:47:12 +0530

    Update alert_sub.sh

--- alert_sub.sh
+++ alert_sub.sh
@@ -5,14 +5,18 @@ cat subop.txt | sort > subop.txt
 
 while true;
 do
- subfinder -silent -d TARGETWEBSITE.com -all | sort > subop1.txt
- diff subop.txt subop1.txt | grep '^<' | sed 's/^< //' > newsubdomains.txt
+ subfinder -silent -dL targetdomainlist.txt -all | sort > subop1.txt
+ diff subop.txt subop1.txt | grep '^>' | sed 's/^> //' > newsubdomains.txt
  subcount=$(cat newsubdomains.txt | wc -l)
 
  if [ "$subcount" -ne 0 ]; then
-  echo "    telegram_format: 'You have $subcount new subdomains for TARGETWEBSITE.com. The new subdomains are: {{data}}'" >> alertconfig.yaml;
-  notify -pc alertconfig.yaml -silent -data newsubdomains.txt  -provider telegram -bulk
+  echo "    telegram_format: 'You have $subcount new subdomains. The new subdomains are: {{data}}'" >> config.yaml && notify -pc config.yaml -silent -data newsubdomains.txt  -provider telegram -bulk;
  fi
+
+ head -n 5 config.yaml > temp.txt && mv temp.txt config.yaml
+ cat newsubdomains.txt >> subop.txt
+ cat subop.txt | sort > subop.txt
+
  ((iterationcount++))
  echo "$iterationcount" > runcount.txt
  sleep 3600

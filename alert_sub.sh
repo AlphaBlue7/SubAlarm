2290d8bba517f05bb3b00f01a9b233ef1d382132
AlphaBlue7 <62275748+AlphaBlue7@users.noreply.github.com>
2023-10-12 23:05:38 +0530

    Update alert_sub.sh

--- alert_sub.sh
+++ alert_sub.sh
@@ -10,7 +10,7 @@ do
  subcount=$(cat newsubdomains.txt | wc -l)
 
  if [ "$subcount" -ne 0 ]; then
-  echo "    telegram_format: 'You have $subcount new subdomains for tidal.com. The new subdomains are: {{data}}'" >> alertconfig.yaml;
+  echo "    telegram_format: 'You have $subcount new subdomains for TARGETWEBSITE.com. The new subdomains are: {{data}}'" >> alertconfig.yaml;
   notify -pc alertconfig.yaml -silent -data newsubdomains.txt  -provider telegram -bulk
  fi
  ((iterationcount++))
